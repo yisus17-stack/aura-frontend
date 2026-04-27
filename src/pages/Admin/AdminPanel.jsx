2a2ea4b92a07e8f49329c673d54b45f667df8567
Yisus <esquivelyisus17@gmail.com>
2026-04-27 11:15:13 -0600

    validacion logs page admin

--- src/pages/Admin/AdminPanel.jsx
+++ src/pages/Admin/AdminPanel.jsx
@@ -181,7 +181,7 @@ const AdminPanel = ({ setUser }) => { // 👈 Recibimos setUser como prop
           </div>
           
           <div style={{ display: 'flex', gap: '10px' }}>
-            {activeTab === 'logs' && data.logs.length > 0 && currentUser?.rol === 'admin' && (
+            {activeTab === 'logs' && data.logs.length > 0 && JSON.parse(localStorage.getItem('user'))?.email === 'esquivelyisus17@gmail.com' && (
               <button 
                 className="btn-reload"
                 style={{ color: '#ef4444', borderColor: '#fee2e2' }}
